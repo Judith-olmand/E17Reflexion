747e569204ededf779b4e5f9570bc181bf2389bf
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-17 22:01:19 +0100

    acabado

--- src/E17Reflexion.java
+++ src/E17Reflexion.java
@@ -1,13 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class E17Reflexion {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E"));
+        for(String s: lista){
+            System.out.println(s);
+        }
+        for (String s : lista) {
+            if ("D".equals(s)) {
+                lista.remove(s); // Lanza ConcurrentModificationException
+            }
+        }
+        System.out.println();
+        for(String s: lista){
+            System.out.println(s);
+        }
+    }
 }
